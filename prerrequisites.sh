a0c1890ce43a8f0a7bfcf0ba0f3c9db215726689
Leandro Echevarria <echevarrialeandro@gmail.com>
2020-06-18 06:06:29 +0000

    Fixing prerrequisites by adding an update of the mirrors.

--- prerrequisites.sh
+++ prerrequisites.sh
@@ -5,6 +5,19 @@
 
 . helpers.sh # load helper functions
 
+# I use reflector to optimize the mirrors before install the system.
+# https://wiki.archlinux.org/index.php/Reflector
+sudo pacman --noconfirm -S reflector
+
+# Backup mirrors
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+# Run reflector to select the HTTPS mirrors synchronized within the last 24 hours, sort them by download speed, and overwrite the file /etc/pacman.d/mirrorlist:
+sudo reflector --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Update
+sudo pacman -Syy
+
 echo_info "Installing base-devel package..."
 sudo pacman -S base-devel
 
@@ -13,4 +26,4 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 
 echo_info "Building yay..."
-makepkg -si
+makepkg -sir
